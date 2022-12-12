7d3433e73b431ca43901e05a6c943e14e3504acd
WDIH404 <flazy322@gmail.com>
2022-12-13 02:40:43 +0700

    Фикс корзины

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-
-import React from 'react'
+import React from 'react';
 import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
@@ -10,41 +9,49 @@ function App() {
   const [cartOpened, setCartOpened] = React.useState(false);
 
   React.useEffect(() => {
-  fetch('https://635d53cdcb6cf98e56b26681.mockapi.io/items')
-    .then((res) => {
-      return res.json();
-    })
-    .then((json) => {
-      setItems(json);
-    });
+    fetch('https://635d53cdcb6cf98e56b26681.mockapi.io/items')
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setItems(json);
+      });
   }, []);
+
+  const onAddToCart = (obj) => {
+    setCartItems(([...cartItems, obj]));
+  };
+
+  const dellFromCart =
+
+  console.log(cartItems)
 
   return (
     <>
       <div className="wrapper clear">
-      {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)}/>}
-      <Header onClickCart={() => setCartOpened(true)} />
-      <div className="content p-40">
-        <div className="d-flex align-center justify-between mb-40">
-          <h1>Все кроссовки</h1>
-          <div className="search-block d-flex">
-            <img src="/img/search.svg" alt="Search" />
-            <input placeholder="Поиск..." />
+        {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} />}
+        <Header onClickCart={() => setCartOpened(true)} />
+        <div className="content p-40">
+          <div className="d-flex align-center justify-between mb-40">
+            <h1>Все кроссовки</h1>
+            <div className="search-block d-flex">
+              <img src="/img/search.svg" alt="Search" />
+              <input placeholder="Поиск..." />
+            </div>
+          </div>
+          <div className="d-flex flex-wrap" style={{ rowGap: '40px' }}>
+            {items.map((item) => (
+              <Card
+                title={item.title}
+                price={item.price}
+                imageUrl={item.imageUrl}
+                onFavorite={() => console.log('Добавили в закладки')}
+                onPlus={onAddToCart}
+              />
+            ))}
           </div>
         </div>
-        <div className="d-flex flex-wrap" style = {{rowGap: "40px"}}>
-          {items.map((obj) => (
-            <Card
-              title={obj.title}
-              price={obj.price}
-              imageUrl={obj.imageUrl}
-              onFavorite={() => console.log('Добавили в закладки')}
-              onPlus={() => console.log('Нажали плюс')}
-            />
-          ))}
-        </div>
       </div>
-    </div>
     </>
   );
 }
